7fe304a232c2c071b33aa20a7cc8c9ec7272edf5
yongseok <yongseok.dev@gmail.com>
2023-05-14 19:23:21 +0900

    fix: websocket 설정 변경 파비콘 추가

--- src/main/java/site/doget/omok/socket/WebSocketConfig.java
+++ src/main/java/site/doget/omok/socket/WebSocketConfig.java
@@ -1,7 +1,11 @@
 package site.doget.omok.socket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,6 +22,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").withSockJS(); // WebSocket 엔드포인트를 "/chat"으로 설정
+        registry.addEndpoint("/ws").withSockJS();
     }
 }
